9f550bc1b011700be3d71e40191a53605630f1e5
Aliona Shlapak <aliona@milestep.io>
2021-11-02 11:06:18 +0200

    changes_branch2

--- cypress/integration/homePage.spec.js
+++ cypress/integration/homePage.spec.js
@@ -12,11 +12,11 @@ describe('The Search Page', () => {
         cy.contains('Log In').first().click()
         cy.get('[name="email"]').type('alena.shlapak.2000@gmail.com')
         cy.get('[name="password"]').type('Password11+')
-        cy.screenshot('user-login-errors')
+        cy.screenshot('user-login-errors') // makes screenshot
         cy.get ('button[type="submi"]').click()
         cy.url().should('not.include', 'login')
         cy.log('Login successful')
-        cy.screenshot()
+        cy.screenshot() // makes screenshot
         cy.get('.styles_header__searchInput__bz4Gn').type('session')
         cy.get('.styles_header__searchSvg__3ANQa').screenshot().click()
         cy.get('.styles_eventCardTablet__Cr6Iy').first().should('be.visible')
